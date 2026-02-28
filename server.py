e41eca1cba27166991884b038f1bb2fc24a750d7
nitinind91 <nitinind91@gmail.com>
2026-02-28 21:16:36 +0530

    mcp-port-added

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ def greetings(name: str) -> str:
     return f"Hello {name}, Have a nice day"
 
 if __name__ =="__main__":
-    mcp.run(transport="streamable-http")
+    mcp.run(transport="streamable-http", host="0.0.0.0",port=8000)
