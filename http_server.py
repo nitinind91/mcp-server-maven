563047adc4dd924d125e1c773a952a5d94341be8
nitinind91 <nitinind91@gmail.com>
2026-02-28 21:59:46 +0530

    commit

--- http_server.py
+++ http_server.py
@@ -5,7 +5,7 @@ mcp = FastMCP("greetings", host="0.0.0.0",port=8000)
 @mcp.tool()
 def greetings(name: str) -> str:
     "Send greetings"
-    return f"Hello {name}, Have a nice day"
+    return f"Hello {name}, I say Have a nice day"
 
 if __name__ =="__main__":
     mcp.run(transport="streamable-http")
